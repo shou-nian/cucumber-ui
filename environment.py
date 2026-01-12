3801f561284058b79eca0ff84d341bb1ef120a83
riny <riny@aspecta.id>
2026-01-12 14:16:34 +0800

    add log to environment.py

--- environment.py
+++ environment.py
@@ -1,23 +1,34 @@
+import inspect
+
 from config import load_config
 from pkg import BrowserFactory, logger, PageFactory
 
 
 def before_all(context):
+    logger.name = __name__
+    context.logger = logger
+    context.logger.info(f"execute {inspect.currentframe().f_code.co_name}")
+
     browser_config = load_config()
     context.factory = BrowserFactory(browser_config)
 
-    logger.name = __name__
-    context.logger = logger
+
 
 
 def after_all(context):
+    context.logger.info(f"execute {inspect.currentframe().f_code.co_name}")
+
     context.factory.quit()
 
 
 def before_scenario(context, scenario):
+    context.logger.info(f"execute {inspect.currentframe().f_code.co_name}")
+
     driver = context.factory.create_driver()
     context.pages = PageFactory(driver)
 
 
 def after_scenario(context, scenario):
+    context.logger.info(f"execute {inspect.currentframe().f_code.co_name}")
+
     context.pages.reset()
